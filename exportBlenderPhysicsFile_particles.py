36a16af945c63a2b77c613d575e8d4994c1983c1
RH2 <blendblob@gmail.com>
2020-12-27 02:28:10 -0700

    bug
    have the particle count and mask flipped

--- exportBlenderPhysicsFile_particles.py
+++ exportBlenderPhysicsFile_particles.py
@@ -1,15 +1,3 @@
-includeflags = [
-1, #id
-1, #location
-1, #velocity
-1, #rotation
-1, #rotational velocity
-1, #size
-1, #times
-1] #boids
-
-
-
 import hou
 import os
 import numpy
@@ -17,12 +5,31 @@ from struct import *
 node = hou.pwd()
 geo = node.geometry()
 
+
+b_id =  1   #id
+b_loc=  1   #location
+b_vel=  1   #velocity
+b_rot=  1   #rotation
+b_rotv= 1   #rotational velocity
+b_s=    1   #size
+b_t=    1   #times
+b_b=    1   #boids
+
+framespersecond = 25
+
+flagArray = [b_id,b_loc,b_vel,b_rot,b_rotv,b_s,b_t,b_b]
+
+def outMaskGen(input):
+    number = 0
+    for i in range(len(input)):
+        number+=(1<<i)*input[i]
+    return(number)
+
 def makequat(vector):
     #Quaternion q;
     #vector a = crossproduct(v1, v2);
     #q.xyz = a;
     #q.w = sqrt((v1.Length ^ 2) * (v2.Length ^ 2)) + dotproduct(v1, v2);
-
     q = hou.Vector4()
     defaultVector = hou.Vector3( (1.0,0.0,0.0) )
     crossVector = vector.cross(defaultVector)
@@ -33,89 +40,79 @@ def makequat(vector):
     #print "result: ",result
     return result
 
+
 def writeID(f,var):
     data = var.attribValue(id_attrib)
     f.write( pack("I",data))
+
 def writePos(f,var):
-    data = var.attribValue(id_attrib)
+    data = var.attribValue(P_attrib)
     f.write( pack("3f",data[0],data[1],data[2]))
+
 def writeVel(f,var):
     data = var.attribValue(v_attrib)
     f.write( pack("3f",data[0],data[1],data[2]))
+
 def writeAngle(f,var):
     data = makequat( hou.Vector3( var.attribValue(n_attrib) ) )
     f.write( pack("4f",data[0],data[1],data[2],data[3]))
+
 def writeAngleVel(f,var):
     data = hou.Vector3( (0.0,0.0,0.0) ) #todo
     f.write( pack("3f",data[0],data[1],data[2]))
+
 def writeScale(f,var):
     data = var.attribValue(size_attrib)
-    f.write( pack("3f",data[0],data[1],data[2]))
+    f.write( pack("f",data))
+
 def writeTime(f,var):
-    bith = var.attribValue(age_attrib)
-    lifetime = var.attribValue(age_attrib)
-    death = var.attribValue(age_attrib)
+    now = $F/framespersecond
+    age = var.attribValue(age_attrib)
+    lifetime = var.attribValue(life_attrib)
+    birth = now-age
+    death = (lifetime-age)
     f.write( pack("3f",birth,lifetime,death))
+
 def writeBoid(f,var):
     data = hou.Vector4( (0.0,0.0,0.0,0.0) ) #todo
     f.write( pack("4f",data[0],data[1],data[2],data[3]))
-exportLib = [writeID,writePos,writeVel,writeAngle,writeAngleVel,writeScale,writeTime,writeBoid]
+
+packFunctions = [writeID,writePos,writeVel,writeAngle,writeAngleVel,writeScale,writeTime,writeBoid]
 
 #FrameWithLeadingZeros = padzero(5,$F)
 simPrefix = "houdiniSim"
 frame =  "%05d" % ($F,)
 simPostfix= "00"
+extension = ".bphys"
 
 directory = r'A:/sim/blendcache_MANTA2/'
 name =  simPrefix+"_"+frame+"_"+simPostfix
-outPath = directory+name
+outPath = directory+name+extension
 
-print outPath
-with open(outPath, 'wb') as f:
 
 header = pack("8c",'B','P','H','Y','S','I','C','S') #blender physics file!
 header += pack("H",1) #particles only!
 header += pack("H",0) #special flag!
+header += pack("I",outMaskGen(flagArray)) #flag array to unsigned int.
 
-f.write(header)
+print outPath
+with open(outPath, 'wb') as f:
+    f.write(header)
+    contents = ""
 
-contents = ""
-id_attrib = geo.findPointAttrib("id");
-P_attrib = geo.findPointAttrib("P");
-v_attrib = geo.findPointAttrib("v");
-n_attrib = geo.findPointAttrib("N");
-rotV_attrib = 0
-size_attrib = geo.findPointAttrib("pscale");
-age_attrib = geo.findPointAttrib("age");
-boid_attrib = 0
+    ####################    point Attribs!
+    id_attrib = geo.findPointAttrib("id");
+    P_attrib = geo.findPointAttrib("P");
+    v_attrib = geo.findPointAttrib("v");
+    n_attrib = geo.findPointAttrib("N");
+    rotV_attrib = 0.0
+    size_attrib = geo.findPointAttrib("pscale");
+    age_attrib = geo.findPointAttrib("age");
+    life_attrib = geo.findPointAttrib("life");
 
+    boid_attrib = 0.0
 
-for point in geo.points():
-    for i in range(8):
-        exportLib[i](f,point)
-
-
-
-    #I = point.attribValue(id_attrib)
-    #P = point.attribValue(P_attrib) #position
-    #V = point.attribValue(v_attrib) #velocity
-    #A = makequat( hou.Vector3( point.attribValue(n_attrib) ) )
-    #AV= angularVelocity = hou.Vector3( (0.0,0.0,0.0) ) #empty for now
-    #S = point.attribValue(id_attrib)
-    #T = point.attribValue(age_attrib)
-    #B = (0.0, 0.0, 0.0, 0.0, 0.0) #five empty attributes.
-
-
-    #f.write( pack("I",I+1))                     #I
-    #f.write( pack("3f",P[0],P[1],P[2]))         #P
-    #f.write( pack("3f",V[0],V[1],V[2]))         #V
-    #f.write( pack("4f",A[0],A[1],A[2],A[3]))    #Angle
-    #f.write( pack("3f",0,0,0))                  #AngleVelocity
-    #f.write( pack("f",S) )                      #S
-    #f.write( pack("3f", 0.0, 5.0, T))           #T  (start,end,lifetime?)
-
-    #f.write( pack("f",0))                       #BOID
-    #f.write( pack("f",0))
-    #f.write( pack("f",0))
-    #f.write( pack("f",0))
-    #f.write( pack("f",0))
+    for point in geo.points():
+        for i in range(len(flagArray)):
+            if(flagArray[i]==1):
+                packFunctions[i](f,point)
