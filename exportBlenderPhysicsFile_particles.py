9e0d0b0e003caecff297dead72245191e324a5b1
RH2 <blendblob@gmail.com>
2020-12-27 00:36:35 -0700

    Adding Section Mask
    Idea is to only run relevant point attribute packing using a bitmask.
    
    Controlling variables are at the top of the file for easy access.

--- exportBlenderPhysicsFile_particles.py
+++ exportBlenderPhysicsFile_particles.py
@@ -1,20 +1,14 @@
-#'BPHYSICS'+
-#0 softbody
-#1 particles
-#2 cloth
-#3 smoke_domain
-#4 smoke_highres
-#5 dynamicpaint
-#6 rigidbody
+includeflags = [
+1, #id
+1, #location
+1, #velocity
+1, #rotation
+1, #rotational velocity
+1, #size
+1, #times
+1] #boids
 
-#index , 4          (4)
-#location , 12      (4,4,4)
-#velocity , 12      (4,4,4)
-#rotation , 16      (4,4,4,4)
-#avelocity , 12     (4,4,4)
-#size , 4           (4)
-#times , 12         (4,4,4)
-#boids , 20         (4,4,4,4,4) LOTS OF EMPTY SPACE!
+
 
 import hou
 import os
@@ -39,7 +33,33 @@ def makequat(vector):
     #print "result: ",result
     return result
 
-
+def writeID(f,var):
+    data = var.attribValue(id_attrib)
+    f.write( pack("I",data))
+def writePos(f,var):
+    data = var.attribValue(id_attrib)
+    f.write( pack("3f",data[0],data[1],data[2]))
+def writeVel(f,var):
+    data = var.attribValue(v_attrib)
+    f.write( pack("3f",data[0],data[1],data[2]))
+def writeAngle(f,var):
+    data = makequat( hou.Vector3( var.attribValue(n_attrib) ) )
+    f.write( pack("4f",data[0],data[1],data[2],data[3]))
+def writeAngleVel(f,var):
+    data = hou.Vector3( (0.0,0.0,0.0) ) #todo
+    f.write( pack("3f",data[0],data[1],data[2]))
+def writeScale(f,var):
+    data = var.attribValue(size_attrib)
+    f.write( pack("3f",data[0],data[1],data[2]))
+def writeTime(f,var):
+    bith = var.attribValue(age_attrib)
+    lifetime = var.attribValue(age_attrib)
+    death = var.attribValue(age_attrib)
+    f.write( pack("3f",birth,lifetime,death))
+def writeBoid(f,var):
+    data = hou.Vector4( (0.0,0.0,0.0,0.0) ) #todo
+    f.write( pack("4f",data[0],data[1],data[2],data[3]))
+exportLib = [writeID,writePos,writeVel,writeAngle,writeAngleVel,writeScale,writeTime,writeBoid]
 
 #FrameWithLeadingZeros = padzero(5,$F)
 simPrefix = "houdiniSim"
@@ -51,9 +71,13 @@ name =  simPrefix+"_"+frame+"_"+simPostfix
 outPath = directory+name
 
 print outPath
+with open(outPath, 'wb') as f:
 
 header = pack("8c",'B','P','H','Y','S','I','C','S') #blender physics file!
-header += pack("I",1) #particles only!
+header += pack("H",1) #particles only!
+header += pack("H",0) #special flag!
+
+f.write(header)
 
 contents = ""
 id_attrib = geo.findPointAttrib("id");
@@ -64,33 +88,34 @@ rotV_attrib = 0
 size_attrib = geo.findPointAttrib("pscale");
 age_attrib = geo.findPointAttrib("age");
 boid_attrib = 0
+
+
 for point in geo.points():
-    #index , 4          (4)
-    #location , 12      (4,4,4)
-    #velocity , 12      (4,4,4)
-    #rotation , 16      (4,4,4,4)
-    #avelocity , 12     (4,4,4)
-    #size , 4           (4)
-    #times , 12         (4,4,4)
-    #boids , 20         (4,4,4,4,4) LOTS OF EMPTY SPACE!
-    I = point.attribValue(id_attrib)
-    P = point.attribValue(P_attrib) #position
-    V = point.attribValue(v_attrib) #velocity
-    #A = makequat( hou.Vector3( (0.5,0.2,0.1) ) )
-    A = makequat( hou.Vector3( point.attribValue(n_attrib) ) )
-    AV= angularVelocity = hou.Vector3( (0.0,0.0,0.0) ) #empty for now
-    S = point.attribValue(id_attrib)
-    T = point.attribValue(age_attrib)
-    B = (0.0, 0.0, 0.0, 0.0, 0.0) #five empty attributes.
+    for i in range(8):
+        exportLib[i](f,point)
 
-    #print I,P,V,A,AV,S,T,B
 
-    #pos2 = hou.vector3(P)
-    #print pos2
 
-    #contents += pack("I",I)
-    #contents += pack("f",P.x)
-    #contents += pack("f",P.y)
-    #contents += pack("f",P.z)
+    #I = point.attribValue(id_attrib)
+    #P = point.attribValue(P_attrib) #position
+    #V = point.attribValue(v_attrib) #velocity
+    #A = makequat( hou.Vector3( point.attribValue(n_attrib) ) )
+    #AV= angularVelocity = hou.Vector3( (0.0,0.0,0.0) ) #empty for now
+    #S = point.attribValue(id_attrib)
+    #T = point.attribValue(age_attrib)
+    #B = (0.0, 0.0, 0.0, 0.0, 0.0) #five empty attributes.
 
-    #print contents
+
+    #f.write( pack("I",I+1))                     #I
+    #f.write( pack("3f",P[0],P[1],P[2]))         #P
+    #f.write( pack("3f",V[0],V[1],V[2]))         #V
+    #f.write( pack("4f",A[0],A[1],A[2],A[3]))    #Angle
+    #f.write( pack("3f",0,0,0))                  #AngleVelocity
+    #f.write( pack("f",S) )                      #S
+    #f.write( pack("3f", 0.0, 5.0, T))           #T  (start,end,lifetime?)
+
+    #f.write( pack("f",0))                       #BOID
+    #f.write( pack("f",0))
+    #f.write( pack("f",0))
+    #f.write( pack("f",0))
+    #f.write( pack("f",0))
